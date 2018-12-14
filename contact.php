c993daaeac7bee4f4fb4983c6f07244e4c72a0bf
dejanrojin <dejan.rojin@yahoo.com>
2018-12-14 20:37:40 +0100

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php 
+
+$username=$_POST["fname"];
+$email=$_POST["email"];
+
+echo $username;
+echo $email;
+
+$text="";
+
+$text=$text."Ime: ".$username."\n";
+$text=$text."Email: ".$email."\n";
+
+
+mail("dejanpavlov91@gmail.com","Poruka Tesla sajt", $text);
+header("Location:success.php?status=thanks");
+
+
+
+?>
